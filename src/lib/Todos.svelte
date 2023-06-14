c35563e998d5e3758530b7c9365b52c6495ec576
comar1 <officialfranciscomar@gmail.com>
2023-06-14 20:13:40 +0800

    no delete functionality

---
+++ src/lib/Todos.svelte
@@ -0,0 +1,30 @@
+<script>
+  let todos = [];
+  let task = "";
+
+  const addTodo = () => {
+    let todo = {
+      task: "",
+      isComplete: false,
+      createdAt: new Date(),
+    };
+    todos = [...todos, todo];
+  };
+
+  $: console.table(todo);
+</script>
+
+<input type="text" bind:value={todo.task} />
+<button on:click={addTodo}>Add</button>
+
+<ol>
+  {#each todos as todo}
+    <li class:complete={todo.isComplete}>{todo.task}</li>
+  {/each}
+</ol>
+
+<style>
+  .complete {
+    text-decoration: line-through;
+  }
+</style>
